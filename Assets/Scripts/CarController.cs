3944c7d5ff139505a68c88396d65eb32a05c1e9a
Mohamed Hesham <mohamed.hesham@outlook.com>
2017-08-01 04:15:10 +0200

    Balanced_Car
    Initial Commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    public float speed = 1500;
+    public float rotationSpeed = 600f;
+    private float movement = 0f;
+    private float rotation = 0f;
+    public WheelJoint2D backWheel;
+    public WheelJoint2D frontWheel;
+    public int maxMotorTorque = 10000;
+    public Rigidbody2D rB;
+
+    private void Update()
+    {
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+        //movement = -Input.GetAxisRaw("Vertical") * speed;
+        Move(-Input.GetAxisRaw("Vertical"));
+        //rotation = -Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.fixedDeltaTime;
+        Rotation(-Input.GetAxisRaw("Horizontal"));
+#endif
+    }
+
+    private void FixedUpdate()
+    {
+        if (movement == 0f)
+        {
+            backWheel.useMotor = false;
+            frontWheel.useMotor = false;
+        }
+        else
+        {
+            backWheel.useMotor = true;
+            frontWheel.useMotor = true;
+            JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = maxMotorTorque };
+            backWheel.motor = motor;
+            frontWheel.motor = motor;
+        }
+
+        rB.AddTorque(rotation);
+
+
+    }
+
+    public void Move(float moveInput)
+    {
+        movement = speed * moveInput;
+    }
+    public void Rotation(float rotationValue)
+    {
+        rotation = rotationValue * rotationSpeed * Time.fixedDeltaTime;
+    }
+
+}
+    
---
+++ Assets/Scripts/TouchControllers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControllers : MonoBehaviour {
+
+    private CarController theCar;
+    
+	// Use this for initialization
+	void Start () {
+        theCar = FindObjectOfType<CarController>();
+	}
+
+    public void LeftArrow()
+    {
+        theCar.Move(1);
+    }
+    public void RightArrow()
+    {
+        theCar.Move(-1);
+    }
+    public void UnpressedArraow()
+    {
+        theCar.Move(0);
+    }
+    public void RotateRight()
+    {
+        theCar.Rotation(-1);
+    }
+    public void RotateLeft()
+    {
+        theCar.Rotation(1);
+    }
+    public void UnRotate()
+    {
+        theCar.Rotation(0);
+    }
+}
